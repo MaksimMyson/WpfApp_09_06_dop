33b45cb9d21978398c2b51979bac547d991eac95
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-09 14:04:23 +0300

    was done hm3

--- WpfApp_09_06_dop/MainWindow.xaml.cs
+++ WpfApp_09_06_dop/MainWindow.xaml.cs
@@ -11,14 +11,32 @@ using System.Windows.Shapes;
 
 namespace WpfApp_09_06_dop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private const string SemaphoreName = "MyAppSemaphore";
+        private Semaphore semaphore;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            // Try to create a new semaphore. If it already exists, it means another instance is running.
+            bool createdNew;
+            semaphore = new Semaphore(3, 3, SemaphoreName, out createdNew);
+
+            if (!createdNew)
+            {
+                // Another instance is running. Show message and close this instance.
+                MessageBox.Show("Another instance of this application is already running.", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+                Close();
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            // Release the semaphore when the window is closed.
+            semaphore.Release();
+            base.OnClosed(e);
         }
     }
 }
