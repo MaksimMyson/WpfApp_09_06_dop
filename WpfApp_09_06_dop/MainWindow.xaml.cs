b407890e64125517dab17edd950f09cc9c25ff28
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-09 14:02:13 +0300

    was done ex2

--- WpfApp_09_06_dop/MainWindow.xaml.cs
+++ WpfApp_09_06_dop/MainWindow.xaml.cs
@@ -11,14 +11,63 @@ using System.Windows.Shapes;
 
 namespace WpfApp_09_06_dop
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private static Mutex mutex = new Mutex();
+        private static bool isFirstThreadFinished = false;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            Thread firstThread = new Thread(PrintAscending);
+            Thread secondThread = new Thread(PrintDescending);
+
+            firstThread.Start();
+            secondThread.Start();
+        }
+
+        private void PrintAscending()
+        {
+            mutex.WaitOne();
+            try
+            {
+                for (int i = 0; i <= 20; i++)
+                {
+                    Dispatcher.Invoke(() => AscendingTextBlock.Text += i + "\n");
+                    Thread.Sleep(100); // Для демонстрації роботи потоків
+                }
+                isFirstThreadFinished = true;
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+
+        private void PrintDescending()
+        {
+            while (!isFirstThreadFinished)
+            {
+                Thread.Sleep(100); // Очікування завершення першого потоку
+            }
+
+            mutex.WaitOne();
+            try
+            {
+                for (int i = 10; i >= 0; i--)
+                {
+                    Dispatcher.Invoke(() => DescendingTextBlock.Text += i + "\n");
+                    Thread.Sleep(100); // Для демонстрації роботи потоків
+                }
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
         }
     }
 }
