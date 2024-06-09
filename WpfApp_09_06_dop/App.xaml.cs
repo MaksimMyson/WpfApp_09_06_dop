fb792b25dc31bb673ed4786230399c80392773c5
Mayson Kotlyar Maksim <maksimmyson33@gmail.com>
2024-06-09 13:57:50 +0300

    Add project files.

---
+++ WpfApp_09_06_dop/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_09_06_dop
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
